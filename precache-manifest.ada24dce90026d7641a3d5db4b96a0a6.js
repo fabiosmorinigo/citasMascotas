fc22028ee3a542e5540cf72f3b351d7938c849ba
fabiosmorinigo <fabio_sosapy@outlook.com>
2022-08-27 16:16:48 -0400

    Updates

---
+++ precache-manifest.ada24dce90026d7641a3d5db4b96a0a6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "2d2b7396805f6e365d95",
+    "url": "/administradorDeCitasVeterinaria/static/css/main.f04e7a80.chunk.css"
+  },
+  {
+    "revision": "2d2b7396805f6e365d95",
+    "url": "/administradorDeCitasVeterinaria/static/js/main.2d2b7396.chunk.js"
+  },
+  {
+    "revision": "b1afefa29afa4e553c2b",
+    "url": "/administradorDeCitasVeterinaria/static/js/1.b1afefa2.chunk.js"
+  },
+  {
+    "revision": "28211d4e492f21424a06",
+    "url": "/administradorDeCitasVeterinaria/static/js/runtime~main.28211d4e.js"
+  },
+  {
+    "revision": "26999bb8e3da4498980ebf9ec8b62ef3",
+    "url": "/administradorDeCitasVeterinaria/static/media/background.26999bb8.jpg"
+  },
+  {
+    "revision": "beaeab17883fa2c65934facc5624dd14",
+    "url": "/administradorDeCitasVeterinaria/static/media/logo.beaeab17.png"
+  },
+  {
+    "revision": "636132c35ecd3a4969fcadcd4029210c",
+    "url": "/administradorDeCitasVeterinaria/index.html"
+  }
+];
