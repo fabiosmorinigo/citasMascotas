157b3845c0da5add419114b1aff7427b372da16b
fabiosmorinigo <fabio_sosapy@outlook.com>
2022-08-27 16:39:11 -0400

    test fix

---
+++ build/precache-manifest.f0a0b05fba78281b3d692f6c6cbe1998.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "94b3a14848e658b16d0a",
+    "url": "/citasMascotas/static/css/main.944bfe65.chunk.css"
+  },
+  {
+    "revision": "94b3a14848e658b16d0a",
+    "url": "/citasMascotas/static/js/main.94b3a148.chunk.js"
+  },
+  {
+    "revision": "ec11ac88537282f30eaf",
+    "url": "/citasMascotas/static/js/1.ec11ac88.chunk.js"
+  },
+  {
+    "revision": "0965e472537d85333b33",
+    "url": "/citasMascotas/static/js/runtime~main.0965e472.js"
+  },
+  {
+    "revision": "26999bb8e3da4498980ebf9ec8b62ef3",
+    "url": "/citasMascotas/static/media/background.26999bb8.jpg"
+  },
+  {
+    "revision": "5c760118471cfad611ff132078625c64",
+    "url": "/citasMascotas/index.html"
+  }
+];
